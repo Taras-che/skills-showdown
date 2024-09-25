88e37f1b5ae42169ae6a00b5350fbfcb423e3b3b
taras <t.cherleniuk@spd-ukraine.com>
2024-09-25 17:18:48 +0300

    Add notification alert

---
+++ src/app/shared/notification-alert/notification-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-notification-alert',
+  standalone: true,
+  imports: [],
+  template: '{{data}}'
+})
+export class NotificationAlertComponent {
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: string) { }
+}
