92258b89ca8b7ec970af76a1acc7068d5a57052d
taras <t.cherleniuk@spd-ukraine.com>
2024-09-27 11:07:28 +0300

    Add confirm dialog

---
+++ src/app/shared/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,40 @@
+import { Component, Inject, Optional } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { ConfirmDialog } from './confirm-dialog.model';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss'
+})
+export class ConfirmDialogComponent {
+  public title: string;
+  public mainText: string | undefined;
+  public isDanger: boolean | undefined;
+  public isWarning: boolean | undefined;
+  public yesButton: string;
+  public noButton: string;
+
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Optional() @Inject(MAT_DIALOG_DATA) public confirmData: ConfirmDialog,
+  ) {
+    this.title = this.confirmData.title;
+    this.mainText = this.confirmData.mainText;
+    this.yesButton = this.confirmData.yesButton;
+    this.noButton = this.confirmData.noButton;
+    this.isDanger = this.confirmData.isDanger;
+    this.isWarning = this.confirmData.isWarning;
+  }
+
+  yesDialog() {
+    this.dialogRef.close('yes-option' );
+  }
+
+  noDialog() {
+    this.dialogRef.close( 'no-option' );
+  }
+}
---
+++ src/app/shared/confirm-dialog/confirm-dialog.model.ts
@@ -0,0 +1,13 @@
+export type ConfirmDialog = {
+ title: string;
+ mainText?: string;
+ isDanger?: boolean;
+ isWarning?: boolean;
+ yesButton: string;
+ noButton: string;
+}
+
+export enum DialogResponse {
+  Yes = 'yes-option',
+  No = 'no-option',
+}
